bba7247a6c18ba64ce2357220a393bd87a424d5d
hasan <mdhasanhk.98@gmail.com>
2022-07-26 15:54:08 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,16 @@
+const submitBtn = document.getElementById('submit-btn');
+submitBtn.addEventListener('click', function(){
+    const inputBtn = document.getElementById('input-btn').value;
+    fetch('https://api.openweathermap.org/data/2.5/weather?q='+inputBtn+'&appid=337d69ee4d025ed87c763f8b856ff904')
+    .then(Response => Response.json())
+    .then(data => {
+        const cityName = data.name;
+        const tem = data.main.temp;
+        const temp = tem - 273.15;
+        const description = data.weather[0].description;
+
+        document.getElementById('city').innerText = cityName;
+        document.getElementById('temp').innerText = temp.toFixed(2);
+        document.getElementById('description').innerText = description;
+    })
+})
